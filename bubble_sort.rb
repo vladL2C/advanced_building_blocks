95d37aed4377ec3e54213635f4b2c130c34ae4d1
astravisual <vlad@hotmail.co.nz>
2017-06-23 22:25:03 -0700

    added bubble_sort_by

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,7 +1,5 @@
 def bubble_sort(array)
-   
-  
-  loop do 
+   loop do 
   i = 0
    swap = false 
   
@@ -20,4 +18,25 @@ def bubble_sort(array)
  return array 
 end 
 
-puts bubble_sort([9,7,2,1,3])
+#puts bubble_sort([9,7,2,1,3])
+
+def bubble_sort_by(array)
+	loop do 
+		i = 0 
+		swap = false 
+		while i < array.length - 1 
+			if yield(array[i], array[i+1]) < 0
+			array[i],array[i+1] = array[i+1],array[i]
+			swap = true  
+		end 
+		i += 1 
+		end 
+		break if swap == false 
+	end 
+	puts array 
+end 
+
+
+bubble_sort_by(["hello","hell","vlad","saraev"]) do |r,l|
+	r.length - l.length 
+end 
